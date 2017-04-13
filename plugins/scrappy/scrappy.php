22edc141557fb361766624196ba6db0e3382379c
j4rk0 <jarohamrak@gmail.com>
2017-04-13 11:30:45 +0200

    REST API custom endpoint

--- plugins/scrappy/scrappy.php
+++ plugins/scrappy/scrappy.php
@@ -10,10 +10,29 @@ Original Author: j4rk0
 Original Author URI:
 */
 
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+  die( "You can't do anything by accessing this file directly." );
+}
+
 define('SCRAPPY_DIR', WP_PLUGIN_DIR . '/scrappy');
 
 require_once SCRAPPY_DIR.'/api/scrappyAPI.php';
 require_once SCRAPPY_DIR.'/api/scrappyParser.php';
+require_once SCRAPPY_DIR.'/api/scrappyRest.php';
+
+// REST API hook for custom endpoint.
+add_action( 'rest_api_init', 'register_rest_link_routes' );
+/**
+ * Register our routes with the REST API.
+ *
+ * @since 2.0.0
+ */
+function register_rest_link_routes() {
+  require_once SCRAPPY_DIR.'/api/scrappyRest.php';
+	$link_controller = new Slug_Custom_Route();
+	$link_controller->register_routes('scrappy');
+}
 
 add_action('admin_menu', 'scrappy_menu');
 
@@ -30,17 +49,17 @@ function scrappy_out () {
   foreach (scrappy_get_radios(false) as $radio) {
     echo '<h3>' . $radio->post_title . '</h3>';
     echo scrappy_renderList( get_live_song($radio->ID) );
-    echo '<hr>';    
+    echo '<hr>';
   }
   echo 'Execution time: ' . (time() - $start);
-  
 }
+
 function scrappy_custom_parsers () {
   foreach (scrappy_get_radios(false) as $radio) {
     $render[$radio->ID] = get_live_song($radio->ID);
   }
   ksort ($render);
-  echo scrappy_renderList($render); 
+  echo scrappy_renderList($render);
 }
 add_action('init', 'scrappy');
 
