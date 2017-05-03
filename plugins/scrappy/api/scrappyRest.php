dda8b1cf39a616880cab0418bc77836bdf6103df
j4rk0 <jarohamrak@gmail.com>
2017-05-03 10:50:41 +0200

    removing not necessary stuff

--- plugins/scrappy/api/scrappyRest.php
+++ plugins/scrappy/api/scrappyRest.php
@@ -56,66 +56,6 @@ class Slug_Custom_Route extends WP_REST_Controller {
     if ($data) return new WP_REST_Response( $data, 200 );
     return new WP_Error( 'wrong-radio-id', __( 'No such radio ID', '_onair'), array( 'status' => 500 ) );
   }
-
- 
-  /**
-   * Create one item from the collection
-   *
-   * @param WP_REST_Request $request Full data about the request.
-   * @return WP_Error|WP_REST_Request
-   */
-  public function create_item( $request ) {
- 
-    $item = $this->prepare_item_for_database( $request );
- 
-    if ( function_exists( 'slug_some_function_to_create_item')  ) {
-      $data = slug_some_function_to_create_item( $item );
-      if ( is_array( $data ) ) {
-        return new WP_REST_Response( $data, 200 );
-      }
-    }
- 
-    return new WP_Error( 'cant-create', __( 'message', '_onair'), array( 'status' => 500 ) );
-  }
- 
-  /**
-   * Update one item from the collection
-   *
-   * @param WP_REST_Request $request Full data about the request.
-   * @return WP_Error|WP_REST_Request
-   */
-  public function update_item( $request ) {
-    $item = $this->prepare_item_for_database( $request );
- 
-    if ( function_exists( 'slug_some_function_to_update_item')  ) {
-      $data = slug_some_function_to_update_item( $item );
-      if ( is_array( $data ) ) {
-        return new WP_REST_Response( $data, 200 );
-      }
-    }
- 
-    return new WP_Error( 'cant-update', __( 'message', '_onair'), array( 'status' => 500 ) );
- 
-  }
- 
-  /**
-   * Delete one item from the collection
-   *
-   * @param WP_REST_Request $request Full data about the request.
-   * @return WP_Error|WP_REST_Request
-   */
-  public function delete_item( $request ) {
-    $item = $this->prepare_item_for_database( $request );
- 
-    if ( function_exists( 'slug_some_function_to_delete_item')  ) {
-      $deleted = slug_some_function_to_delete_item( $item );
-      if (  $deleted  ) {
-        return new WP_REST_Response( true, 200 );
-      }
-    }
- 
-    return new WP_Error( 'cant-delete', __( 'message', '_onair'), array( 'status' => 500 ) );
-  }
  
   /**
    * Check if a given request has access to get items
@@ -129,56 +69,6 @@ class Slug_Custom_Route extends WP_REST_Controller {
   }
  
   /**
-   * Check if a given request has access to get a specific item
-   *
-   * @param WP_REST_Request $request Full data about the request.
-   * @return WP_Error|bool
-   */
-  public function get_item_permissions_check( $request ) {
-    return $this->get_items_permissions_check( $request );
-  }
- 
-  /**
-   * Check if a given request has access to create items
-   *
-   * @param WP_REST_Request $request Full data about the request.
-   * @return WP_Error|bool
-   */
-  public function create_item_permissions_check( $request ) {
-    return current_user_can( 'edit_something' );
-  }
- 
-  /**
-   * Check if a given request has access to update a specific item
-   *
-   * @param WP_REST_Request $request Full data about the request.
-   * @return WP_Error|bool
-   */
-  public function update_item_permissions_check( $request ) {
-    return $this->create_item_permissions_check( $request );
-  }
- 
-  /**
-   * Check if a given request has access to delete a specific item
-   *
-   * @param WP_REST_Request $request Full data about the request.
-   * @return WP_Error|bool
-   */
-  public function delete_item_permissions_check( $request ) {
-    return $this->create_item_permissions_check( $request );
-  }
- 
-  /**
-   * Prepare the item for create or update operation
-   *
-   * @param WP_REST_Request $request Request object
-   * @return WP_Error|object $prepared_item
-   */
-  protected function prepare_item_for_database( $request ) {
-    return array();
-  }
- 
-  /**
    * Prepare the item for the REST response
    *
    * @param mixed $item WordPress representation of the item.
@@ -189,30 +79,4 @@ class Slug_Custom_Route extends WP_REST_Controller {
     return $item;
   }
  
-  /**
-   * Get the query params for collections
-   *
-   * @return array
-   */
-  public function get_collection_params() {
-    return array(
-      'page'     => array(
-        'description'        => 'Current page of the collection.',
-        'type'               => 'integer',
-        'default'            => 1,
-        'sanitize_callback'  => 'absint',
-      ),
-      'per_page' => array(
-        'description'        => 'Maximum number of items to be returned in result set.',
-        'type'               => 'integer',
-        'default'            => 10,
-        'sanitize_callback'  => 'absint',
-      ),
-      'search'   => array(
-        'description'        => 'Limit results to those matching a string.',
-        'type'               => 'string',
-        'sanitize_callback'  => 'sanitize_text_field',
-      ),
-    );
-  }
 }
