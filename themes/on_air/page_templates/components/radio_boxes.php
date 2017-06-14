a58933315a446ac5cff329aa86ac7e0d2eb79473
j4rk0 <jarohamrak@gmail.com>
2017-06-14 14:45:49 +0200

    ordering by share

--- themes/on_air/page_templates/components/radio_boxes.php
+++ themes/on_air/page_templates/components/radio_boxes.php
@@ -1,9 +1,21 @@
-<?php $loop = new WP_Query( array( 'post_type' => 'radio', 'posts_per_page' => -1 ) ); ?>
+<?php
+$args = array(
+	'post_type' => 'radio',
+	'posts_per_page' => -1,
+	'meta_key' => 'radio_share',
+	'orderby' => 'meta_value_num',
+	'order' => 'DESC',
+);
+$loop = new WP_Query($args);
+?>
 <div class="radio-boxes">
     <div class="row">
-    <?php while ( $loop->have_posts() ) : $loop->the_post();
-            get_template_part( 'page_templates/components/radio_box' );
-          endwhile; wp_reset_query(); ?>
+				<?php
+				while ($loop->have_posts()) : $loop->the_post();
+					get_template_part('page_templates/components/radio_box');
+				endwhile;
+				wp_reset_query();
+				?>
     </div>
 </div>
 <div class='clearfix'></div>
--- themes/on_air/page_templates/home-page.php
+++ themes/on_air/page_templates/home-page.php
@@ -7,26 +7,7 @@
 
 get_header();
 $evolve_layout = evolve_get_option('evl_layout', '2cl');
-
-/*
-if (evolve_lets_get_sidebar_2() == true):
-    get_sidebar('2');
-endif;
-*/
 ?>
-<!--
-<div class="row">
-  <div class="col-sm-12">
-    <h1 class="entry-title">
-        <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>">
-            <?php
-              echo mb_strtoupper(get_the_title());
-            ?>
-        </a>
-    </h1>
-  </div>
-</div>
--->
 <div class='clearfix'></div>
 <?php
 get_template_part( 'page_templates/components/radio_boxes' );
