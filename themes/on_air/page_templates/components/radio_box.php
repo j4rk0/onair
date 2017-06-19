2a299c29a64982c67640aeb590a0dd63b7c1220f
j4rk0 <jarohamrak@gmail.com>
2017-06-19 12:15:15 +0200

    mobile styling

--- themes/on_air/page_templates/components/radio_box.php
+++ themes/on_air/page_templates/components/radio_box.php
@@ -1,19 +1,10 @@
-<div class="col-lg-3 col-md-4 col-sm-6  col-xs-12">
+<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
     <div class="radio-box">
 				<div class="row">
-						<div class="col-xs-6 col-sm-12">
+						<div class="col-sm-12">
 							<a href="<?php //the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="radio-logo" style="background-image: url(<?php echo get_the_post_thumbnail_url(get_the_ID(), 'thumbnail'); ?>)">
 							</a>
 						</div>
-						<div class="col-xs-6 col-sm-12">
-							<div class="radio-button">
-									<!--<a class="read-more-box btn t4p-button" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">-->
-									<?php $stream = get_post_meta(get_the_ID(), 'stream_128') ? get_post_meta(get_the_ID(), 'stream_128') : ['0' => '#']; ?>
-									<a class="read-more-box btn t4p-button" target="_blank" href="<?php echo $stream[0]; ?>" title="<?php the_title_attribute(); ?>">
-											<?php the_title() ?> <i class="fa fa-chevron-right"></i>
-									</a>
-							</div>
-					</div>
 			  </div>
 				<div class='clearfix'></div>
 				<div class="row radiosong ajax-onload" data-endpoint="wp-json/scrappy/v1/live/<?php the_ID(); ?>/?getimages=1">
@@ -29,6 +20,18 @@
 								</div>
 						</div>
 				</div>
+				<div class="row">
+						<div class="col-sm-12">
+							<div class="radio-button">
+									<!--<a class="read-more-box btn t4p-button" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">-->
+									<?php $stream = get_post_meta(get_the_ID(), 'stream_128') ? get_post_meta(get_the_ID(), 'stream_128') : ['0' => '#']; ?>
+									<a class="read-more-box btn t4p-button" target="_blank" href="<?php echo $stream[0]; ?>" title="<?php the_title_attribute(); ?>">
+											<?php the_title() ?> <i class="fa fa-chevron-right"></i>
+									</a>
+							</div>
+					</div>
+			  </div>
+				<div class='clearfix'></div>
     </div>
 </div>
 
